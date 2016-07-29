b0cf7b8566a384db24b7c49789f4bf0da20201d3
bec1 <bec1mailbox@gmail.com>
2016-07-29 16:18:29 -0400

    new camera settings

--- SaturationIntensityCalibration.m
+++ SaturationIntensityCalibration.m
@@ -1,7 +1,10 @@
-folder='/Users/Zhenjie/Data/2016-05-16/';
-filelist={'05-17-2016_00_00_52_top';'05-17-2016_00_00_00_top';'05-16-2016_23_59_08_top';'05-16-2016_23_58_15_top';'05-16-2016_23_55_06_top';'05-16-2016_23_54_13_top';'05-16-2016_23_53_21_top';'05-16-2016_23_52_28_top';'05-16-2016_23_51_36_top';'05-16-2016_23_50_44_top';'05-16-2016_23_49_51_top';'05-16-2016_23_48_59_top';'05-16-2016_23_48_07_top';'05-16-2016_23_47_14_top';'05-16-2016_23_46_22_top';'05-16-2016_23_45_29_top';'05-16-2016_23_44_37_top';'05-16-2016_23_43_45_top';'05-16-2016_23_40_38_top';'05-16-2016_23_39_21_top';'05-16-2016_23_38_29_top';'05-16-2016_23_37_04_top';'05-16-2016_23_36_12_top';'05-16-2016_23_35_19_top';'05-16-2016_23_34_27_top';'05-16-2016_23_33_35_top';'05-16-2016_23_32_43_top';'05-16-2016_23_31_50_top';'05-16-2016_23_30_41_top';'05-16-2016_23_29_49_top';'05-16-2016_23_28_56_top';'05-16-2016_23_28_04_top';'05-16-2016_23_27_11_top';'05-16-2016_23_25_54_top';'05-16-2016_23_25_01_top';'05-16-2016_23_22_32_top';'05-16-2016_23_21_31_top';'05-16-2016_23_20_38_top';'05-16-2016_23_19_30_top';'05-16-2016_23_18_38_top';'05-16-2016_23_17_45_top';'05-16-2016_23_16_28_top';'05-16-2016_23_15_16_top';'05-16-2016_23_14_10_top';'05-16-2016_23_13_17_top';'05-16-2016_23_12_25_top';'05-16-2016_23_11_32_top'};
-ROI=[140,45,335,480];
+folder='J:\Elder Backup Raw Images\2016\2016-07\2016-07-26\';
+filelist={'07-26-2016_21_21_20_TopA';'07-26-2016_21_21_20_TopB';'07-26-2016_21_20_28_TopA';'07-26-2016_21_20_28_TopB';'07-26-2016_21_19_36_TopA';'07-26-2016_21_19_36_TopB';'07-26-2016_21_18_44_TopA';'07-26-2016_21_18_44_TopB';'07-26-2016_21_17_52_TopA';'07-26-2016_21_17_52_TopB';'07-26-2016_21_16_59_TopA';'07-26-2016_21_16_59_TopB';'07-26-2016_21_16_07_TopA';'07-26-2016_21_16_07_TopB';'07-26-2016_21_15_15_TopA';'07-26-2016_21_15_15_TopB';'07-26-2016_21_12_45_TopA';'07-26-2016_21_12_45_TopB';'07-26-2016_21_11_53_TopA';'07-26-2016_21_11_53_TopB';'07-26-2016_21_11_01_TopA';'07-26-2016_21_11_01_TopB';'07-26-2016_21_10_09_TopA';'07-26-2016_21_10_09_TopB';'07-26-2016_21_09_16_TopA';'07-26-2016_21_09_16_TopB';'07-26-2016_21_08_24_TopA';'07-26-2016_21_08_24_TopB';'07-26-2016_21_07_32_TopA';'07-26-2016_21_07_32_TopB';'07-26-2016_21_06_40_TopA';'07-26-2016_21_06_40_TopB';'07-26-2016_21_04_55_TopA';'07-26-2016_21_04_55_TopB';'07-26-2016_21_04_03_TopA';'07-26-2016_21_04_03_TopB';'07-26-2016_21_01_08_TopA';'07-26-2016_21_01_08_TopB';'07-26-2016_21_00_16_TopA';'07-26-2016_21_00_16_TopB';'07-26-2016_20_59_24_TopA';'07-26-2016_20_59_24_TopB';'07-26-2016_20_58_32_TopA';'07-26-2016_20_58_32_TopB';'07-26-2016_20_57_40_TopA';'07-26-2016_20_57_40_TopB';'07-26-2016_20_56_48_TopA';'07-26-2016_20_56_48_TopB';'07-26-2016_20_55_03_TopA';'07-26-2016_20_55_03_TopB';'07-26-2016_20_54_11_TopA';'07-26-2016_20_54_11_TopB';'07-26-2016_20_53_19_TopA';'07-26-2016_20_53_19_TopB'};
+list=2:2:54;
+filelist=filelist(list);
+warning('off','all')
 %%
+ROI=[360,440,650,1050];
 imglist={};
 woaMean=[];
 
@@ -13,37 +16,64 @@ for i=1:length(filelist)
     woaMean=[woaMean;mean(woa(:))];
 end
 %%
+WOAThreshold1=000;
+WOAThreshold2=inf;
+mask1=woaMean<WOAThreshold1;
+mask2=woaMean>WOAThreshold2;
+mask=mask1 | mask2;
+imglist(mask)=[];
+woaMean(mask)=[];
+
 Z=ROI(2):ROI(4);
 Z=Z';
-Nsat=330;
+Nsat=200;
 Num=[];
+Numlist={i};
 for i=1:length(imglist)
+    woa=img(:,:,2)-img(:,:,3);
     Nmap=AtomNumber(imglist{i},(1.4*10^-6)^2,0.215*10^-12/2, Nsat );
     Nmap=CleanImage(Nmap );
     nz=sum(Nmap,2);
-    nz=TailTailor(nz,Z,100,400);
+    nz=TailTailor(nz,Z,500,950);
     Num=[Num;sum(nz)];
+    Numlist=[Numlist;nz];
     %Num=[Num;sum(Nmap(:))];
 end
 
 scatter(woaMean,Num);
 ylim([0,max(Num)])
 %%
-Nsatlist=linspace(80,600,25);
+Nsatlist=linspace(70,450,60);
 Stdlist=[];
+klist=[];
 for i=1:length(Nsatlist)
     tic
     Numlist=[];
+    woaMeanlist=[];
     for j=1:length(imglist)
+        imgtemp=imglist{j};
         Nmap=AtomNumber(imglist{j},(1.4*10^-6)^2,0.215*10^-12/2, Nsatlist(i) );
-        Nmap=CleanImage(Nmap );
+        Nmap=CleanImage(Nmap);
+        woa=sum(sum(imgtemp(:,:,2)-imgtemp(:,:,3)));
         nz=sum(Nmap,2);
-        nz=TailTailor(nz,Z,100,400);
+        nz=TailTailor(nz,Z,500,950);
+        woaMeanlist=[woaMeanlist;woa];
         Numlist=[Numlist;sum(nz)];
         
     end
+    P=polyfit(woaMeanlist,Numlist,2);
+    klist=[klist;P(1)];
     Stdlist=[Stdlist;std(Numlist)];
     toc
 end  
-
+%%
 scatter(Nsatlist,Stdlist);
+xlabel('Nsat');ylabel('Variance of total atom number');
+
+%%
+scatter(Nsatlist,klist);
+xlabel('Nsat');ylabel('Variance of total atom number');
+
+%%
+plot(Z,Numlist{1}/max(Numlist{1}),Z,Numlist{3}/max(Numlist{3}),Z,Numlist{5}/max(Numlist{5}),Z,Numlist{7}/max(Numlist{7}),Z,Numlist{9}/max(Numlist{9}))
+ylabel('n_{1d} (a.u.)');xlabel('z (pixel)');
